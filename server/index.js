49754cb57adcd8c1f64b3221012ed3a7cce55ad9
aman-webdev <aman.nabi02@gmail.com>
2024-03-05 22:28:28 +0530

    Add cluster

--- server/index.js
+++ server/index.js
@@ -5,22 +5,45 @@ const urlRouter = require("./routes/url.routes.js");
 const mongoConnect = require("./utils/connectToMongoose.js")
 const path = require("path")
 const cors = require("cors")
-
+const cluster = require("cluster")
+const os = require("os")
 config();
 
-mongoConnect(process.env.MONGOOSE_URL).then(()=>console.log('Connected to DB'))
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(cors())
-app.use(express.json())
+const availableCPUs = os.cpus().length
+console.log('Total CPUS ',availableCPUs)
+mongoConnect(process.env.MONGOOSE_URL).then(()=>{
+    
+})
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+  
+    // Fork workers.
+    for (let i = 0; i < availableCPUs; i++) {
+      cluster.fork();
+    }
+  
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+      cluster.fork()
+    });
+  } else {
 
-app.get('/api/test',(req,res)=>{
-    res.send('Hello from Test route')
-}
-)
-app.use("/", urlRouter);
+    const PORT = process.env.PORT || 3000;
+    const app = express();
+    app.use(cors())
+    app.use(express.json())
+    
+    app.get('/health',(req,res)=>{
+        res.status(200).send(`Hello from Test route ${ process.pid}`)
+    }
+    )
+    app.use("/", urlRouter);
+    
+    app.use(errorMiddleware);
+    
+    app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`));
+  }
 
-app.use(errorMiddleware);
 
-app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`));
+
